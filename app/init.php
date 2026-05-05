a482fb11c7b3f8fcc653b3a44d8ceb39e7b5d430
Shlok Kharva <shlokkharva@gmail.com>
2026-05-05 21:02:20 +0530

    Fix: Use absolute paths for requires to solve Vercel path issues

--- app/init.php
+++ app/init.php
@@ -3,13 +3,13 @@
   session_start();
 
   // Load Config
-  require_once 'config/config.php';
+  require_once __DIR__ . '/config/config.php';
 
   // Load Helpers
-  require_once 'helpers/url_helper.php';
-  require_once 'helpers/session_helper.php';
+  require_once __DIR__ . '/helpers/url_helper.php';
+  require_once __DIR__ . '/helpers/session_helper.php';
 
   // Autoload Core Libraries
   spl_autoload_register(function($className){
-    require_once 'core/' . $className . '.php';
+    require_once __DIR__ . '/core/' . $className . '.php';
   });
